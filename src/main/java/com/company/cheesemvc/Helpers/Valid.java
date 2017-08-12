d58f0119b28d30be935f0a26264fef0e980f4cb5
Adam Duhamel <Adam.Duhamel1996@gmail.com>
2017-08-11 21:57:59 -0700

    renamed a variable to make more sense

--- src/main/java/com/company/cheesemvc/Helpers/Valid.java
+++ src/main/java/com/company/cheesemvc/Helpers/Valid.java
@@ -1,25 +1,23 @@
 package com.company.cheesemvc.Helpers;
 
-import java.util.ArrayList;
-
 public class Valid {
 
     static String[] allowed = {"A","E","I","O","U","Y","B","C","D","F","G","H","K","L","M","N"," ","R","S","T",".",",","J","P","Q","V","W","X","Z","!"};
 
 
     public static Boolean validCheese(String cheese) {
-        int yena = 0;
+        int valids = 0;
         for (int i = 0; i < cheese.length(); i++){
             char ch = cheese.toUpperCase().charAt(i);
             for(int j = 0; j < allowed.length; j++){
                 String al = allowed[j];
                 if (al.charAt(0) == ch){
-                    yena++;
+                    valids++;
                     break;
                 }
             }
         }
-        if (yena == cheese.length() && !cheese.equals("")){
+        if (valids == cheese.length() && !cheese.equals("")){
             return true;
         }
 
@@ -27,21 +25,21 @@ public class Valid {
     }
 
     public static Boolean validDescription(String description) {
-        int yena = 0;
+        int valids = 0;
         if (!description.equals("")){
             for (int i = 0; i < description.length(); i++){
                 char ch = description.toUpperCase().charAt(i);
                 for(int j = 0; j < allowed.length; j++){
                     String al = allowed[j];
                     if (al.charAt(0) == ch){
-                        yena++;
+                        valids++;
                         break;
                     }
                 }
             }
         }
 
-        if (yena == description.length()){
+        if (valids == description.length()){
             return true;
         }
 
