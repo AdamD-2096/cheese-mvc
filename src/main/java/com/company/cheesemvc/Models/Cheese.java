0a1fff785b56f2f3e83d23b11c0f10b8e860cf10
Adam Duhamel <Adam.Duhamel1996@gmail.com>
2017-08-13 10:58:01 -0700

    added a cheese.java file to organize cheeses into model

---
+++ src/main/java/com/company/cheesemvc/Models/Cheese.java
@@ -0,0 +1,29 @@
+package com.company.cheesemvc.Models;
+
+public class Cheese {
+    private String name;
+    private String info;
+
+    public Cheese(String aName, String aInfo) {
+        this.name = aName;
+        this.info = aInfo;
+    }
+    public Cheese(String aName) {
+        this(aName, "");
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    protected void setName(String aName) {
+        this.name = aName;
+    }
+    public String getInfo() {
+        return this.info;
+    }
+
+    protected void setInfo(String aInfo) {
+        this.name = aInfo;
+    }
+}
