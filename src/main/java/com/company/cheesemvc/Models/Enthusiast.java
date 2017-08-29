a6e5a1268380748dd1ba604c9be67884c9f6db82
Adam Duhamel <Adam.Duhamel1996@gmail.com>
2017-08-28 19:31:11 -0700

    Fields/Getters/Setters/Constructors created

--- src/main/java/com/company/cheesemvc/Models/Enthusiast.java
+++ src/main/java/com/company/cheesemvc/Models/Enthusiast.java
@@ -1,4 +1,45 @@
 package com.company.cheesemvc.Models;
 
 public class Enthusiast {
+    /*
+    *   Fields
+    */
+    private String username;
+    private String email;
+    private String password;
+    private int id;
+    private static int nextId = 1000000;
+    /*
+    *   Constructors
+    */
+    public Enthusiast(String username, String email, String password) {
+        this();
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+    public Enthusiast(){
+        this.id = nextId;
+        nextId++;
+    }
+    /*
+    *   Getters and Setters
+    */
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public int getId() { return id; }
+    private void setId(int id) { this.id = id; }
+
+    public static int getNextId() { return nextId; }
+    private static void setNextId(int nextId) { Enthusiast.nextId = nextId; }
+    /*
+    *   Instance Methods
+    */
 }
